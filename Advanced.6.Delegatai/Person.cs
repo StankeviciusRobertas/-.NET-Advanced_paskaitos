37a190587a7d1594e769d62431bffef34fc0eab5
Robertas Stankevičius <stankevic.robertas@gmail.com>
2023-10-19 20:39:10 +0300

    delegatai

---
+++ Advanced.6.Delegatai/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced._6.Delegatai
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
--- Advanced.6.Delegatai/Program.cs
+++ Advanced.6.Delegatai/Program.cs
@@ -7,7 +7,7 @@
         private delegate string NameBuilder(string firstName, string lastName, int age);
         private delegate int TwoNumbers(int number1, int number2);
         private delegate List<int> ElementSelect(List<int> numbers, int step);        
-
+        private delegate bool Filter(Person person);
         static void Main(string[] args)
         {
             //Console.WriteLine("=================================================");
@@ -24,7 +24,7 @@
             //NumberChanger numberChanger;
             //Console.WriteLine("Ar jus norite prideti (+) ar atimti (-)");
             //string userSelection = Console.ReadLine();
-            //if(userSelection == "+")
+            //if (userSelection == "+")
             //{
             //    numberChanger = new NumberChanger(AddNumber);
             //}
@@ -48,7 +48,7 @@
             //Console.WriteLine("=================================================");
 
             //List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            
+
             //ElementSelect elementSelector = ReturnInts;
 
             //List<int> selectedElement = elementSelector(numbers, 2);
@@ -59,9 +59,119 @@
             //    Console.WriteLine(element);
             //}
 
+            //Console.WriteLine("====================== ANONIMINIAI DELEGATAI ===========================");
+
+            //NumberChanger numberChangerAnonymous = delegate (int number)
+            //{
+            //    return number + 5;
+            //};
+            //Console.WriteLine(numberChangerAnonymous(5));
+
+            //Console.WriteLine("=================================================");
+
+            //var x = 8;
+
+            //NumberChanger numberChangerAnonymous = delegate (int number)
+            //{
+            //    return number + x;
+            //};
+            //Console.WriteLine(numberChangerAnonymous(5));
+
+
+            //Console.WriteLine("=================================================");
+
+            //var x = 5;
+            //ExecuteNumberChangerWithValue(x, AddNumber);
+
+            //Console.WriteLine("=================================================");
+
+            //NameBuilder nameBuilder = delegate (string name, string surname, int age)
+            //{
+            //    return name + " " + surname + " " + age;
+            //};
+            //Console.WriteLine(nameBuilder("Robertas", "Stankevicius", 34));
+
+            //Console.WriteLine("=================================================");
+
+            //TwoNumbers twoNumbers = delegate (int number, int number2)
+            //{
+            //    return number + number2;
+            //};
+
+            //Console.WriteLine(twoNumbers(10, 50));
+
+            Console.WriteLine("=================================================");
+
+            List<Person> listOfPersons = new List<Person>
+            {
+                new Person { Name = "Robertas", Age = 8 },
+                new Person { Name = "Karolis", Age = 25 },
+                new Person { Name = "Marijonas", Age = 55 },
+                new Person { Name = "Kazimieras", Age = 66 }                
+            };
+
+            Filter isChild = delegate (Person persons)
+            {
+                if (persons.Age < 18)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }                
+            };
+
+            Filter isAdult = delegate (Person persons)
+            {
+                if (persons.Age >= 18 && persons.Age < 65)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            };
+
+            Filter isSenior = delegate (Person persons)
+            {
+                if (persons.Age >= 65)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            };
+
+
+            DisplayPeople("Children: ", listOfPersons, isChild);
+            DisplayPeople("Adults: ", listOfPersons, isAdult);
+            DisplayPeople("Seniors: ", listOfPersons, isSenior);
+
             Console.WriteLine("=================================================");
         }
 
+
+        static void DisplayPeople(string title, List<Person> people, Filter filter)
+        {
+            Console.WriteLine(title);
+            foreach(var person in people)
+            {
+                if (filter(person))
+                {
+                    Console.WriteLine($"{person.Name}, {person.Age} years old");
+                }
+                Console.WriteLine();
+            }
+        }
+        private static void ExecuteNumberChangerWithValue(int val, NumberChanger numberChanger)
+        {
+            val += 10;
+            numberChanger(val);
+        }
         public static List<int> ReturnInts(List<int> list, int step)
         {
             List<int> selectedElements = new List<int>();
